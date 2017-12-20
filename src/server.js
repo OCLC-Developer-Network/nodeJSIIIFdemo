faf79749af064ba8791c92ebeb973edc992410d5
Coombs,Karen <coombsk@oclc.org>
2017-12-19 23:06:42 -0600

    Fixed async/await issues

--- src/server.js
+++ src/server.js
@@ -3,27 +3,22 @@ const express = require('express');
 const app = express();
 const axios = require("axios");
 class Image {
-    constructor(collectionID, objectID) {
-		this.collectionID = collectionID
-		this.objectID = objectID
-		const url = 'https://sandbox.contentdm.oclc.org/digital/iiif-info/' + collectionID + '/' + objectID;
-		const getManifest = async url => {
+    constructor(data) {
+	    this.manifest = data;
+		this.imageID = this.manifest.sequences[0].canvases[0].images[0].resource.service['@id'];
+		this.metadata = this.manifest.metadata;
+    }
+    static async getImage(collectionID, objectID) {
+    	const url = 'https://sandbox.contentdm.oclc.org/digital/iiif-info/' + collectionID + '/' + objectID;
 		  try {
 		    const response = await axios.get(url);
-		    const data = response.data;
-		    return data;
+		    const data = await response.data;
+		    return await new Image(data);
 		  } catch (error) {
 		    console.log(error);
 		  }
-		};
-        // go get the manifest
-    	this.manifest = getManifest(url);
-		console.log(this.manifest);
-		this.imageID = this.manifest.sequences[0].canvases[0].images[0].resource.service['@id'];
-		this.metadata = this.manifest.metadata;
-    	
     }
-    
+
     getImageID(){
     	return this.imageID;
     }
@@ -50,8 +45,8 @@ app.get('/', (req, res) => {
    res.render('index');
 });
  
-app.post('/submit', (req, res) => {
-	const image = new Image(req.body.collectionID, req.body.objectID)
+app.post('/submit', async (req, res) => {
+	const image = await Image.getImage(req.body.collectionID, req.body.objectID);
     res.render('display-image', { metadata: image.getMetadata(), imageID: image.getImageID() });
 });
 
