da376de3f4090d7aa039e0e1db2cf5b058266598
Coombs,Karen <coombsk@oclc.org>
2018-03-06 14:38:54 -0600

    Added more mocks

---
+++ test/mocks/canvasResponse.js
@@ -0,0 +1,27 @@
+module.exports = { 
+		"height": 4069,
+        "images": [
+          {
+            "motivation": "sc:painting",
+            "on": "https://localhost:8000/manifest/f1de5a8a-0ddf-11e8-ba89-0ed5f89f718b/data/canvas/c1",
+            "resource": {
+              "format": "image/jpeg",
+              "service": {
+                "profile": "http://iiif.io/api/image/2/level1.json",
+                "@context": "http://iiif.io/api/image/2/context.json",
+                "@id": "https://cdm10010.contentdm.oclc.org/digital/iiif/coll16/390"
+              },
+              "height": 4069,
+              "width": 5041,
+              "@id": "https://cdm10010.contentdm.oclc.org/digital/iiif/coll16/390/full/full/0/default.jpg",
+              "@type": "dctypes:Image"
+            },
+            "@id": "https://localhost:8000/manifest/f1de5a8a-0ddf-11e8-ba89-0ed5f89f718b/data/annotation/a1",
+            "@type": "oa:Annotation"
+          }
+        ],
+        "label": "A coasting party on the Cripple Creek Short Line",
+        "width": 5041,
+        "@type": "sc:Canvas",
+        "@id": "https://localhost:8000/manifest/f1de5a8a-0ddf-11e8-ba89-0ed5f89f718b/data/canvas/c1"
+};
